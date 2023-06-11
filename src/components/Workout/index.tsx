2515af45735619ae20547a68580f626d0943f91e
neb <habbouche.matthew@gmail.com>
2023-06-11 18:18:06 -0400

    Add sets

--- src/components/Workout/index.tsx
+++ src/components/Workout/index.tsx
@@ -28,19 +28,30 @@ export function Workout() {
     <>
       <Title>{name}</Title>
       <div className={styles.routine}>
-        {workout.map(({ name: pair, exercises }) => (
-          <div className={styles.group} key={pair}>
-            <h3 className={styles.title}>{pair}</h3>
-            {exercises.map(({ progression, exercise }) => (
-              <Exercise
-                progression={progression}
-                exercise={exercise}
-                currentWorkout={currentWorkoutRef}
-                key={progression + exercise}
-              />
-            ))}
-          </div>
-        ))}
+        {workout.map(({ name: pair, sets, exercises }) => {
+          const setsArr = Array.from(Array(sets).keys());
+          return (
+            <div className={styles.group} key={pair}>
+              <h3 className={styles.title}>{pair}</h3>
+              {setsArr.map((set) => (
+                <div
+                  className={`${styles.set} ${
+                    set + 1 === sets ? styles.last : ""
+                  }`}
+                >
+                  {exercises.map(({ progression, exercise }) => (
+                    <Exercise
+                      progression={progression}
+                      exercise={exercise}
+                      currentWorkout={currentWorkoutRef}
+                      key={progression + exercise + set}
+                    />
+                  ))}
+                </div>
+              ))}
+            </div>
+          );
+        })}
         <button
           className={styles.end}
           onClick={() => console.log(currentWorkout)}
