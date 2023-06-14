38999046b8b43ed244ff610840358325c8316742
neb <habbouche.matthew@gmail.com>
2023-06-13 21:03:07 -0400

    Add android app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.nebz.app',
-  appName: 'nebzapp',
-  webDir: 'dist',
+  appId: "ngo.doflami.nebzapp",
+  appName: "nebzapp",
+  webDir: "dist",
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: "https",
+  },
 };
 
 export default config;
