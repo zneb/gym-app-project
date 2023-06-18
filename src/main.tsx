1bb67caa958e1ba0c1b5d7c4f7223e30e4c04408
neb <habbouche.matthew@gmail.com>
2023-06-18 10:36:40 -0400

    Update sync

--- src/main.tsx
+++ src/main.tsx
@@ -37,7 +37,7 @@ const router = createBrowserRouter([
   },
 ]);
 
-await database.seedDatabase();
+database.seedDatabase();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
